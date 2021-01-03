c0c430eb4e9f137b3998b82dae71171d3e05527a
Adrian Miskell <adrianmi@avaya.com>
2021-01-03 22:18:27 +0000

    added bootstrap added assignment 5: zippy comp using ngcontent ngclass and ngIf

--- src/app/zippy-assignment/zippy-assignment.component.ts
+++ src/app/zippy-assignment/zippy-assignment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-zippy-assignment',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zippy-assignment.component.scss']
 })
 export class ZippyAssignmentComponent implements OnInit {
-
+  @Input('title') title: string;
+  selected: boolean;
   constructor() { }
-
+  toggle() { this.selected = !this.selected; }
   ngOnInit(): void {
+    this.selected = false;
   }
 
 }
